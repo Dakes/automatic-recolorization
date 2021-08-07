ff40942f3cbe47e2282e47d21324f75595cf8a13
Dakes <dakes@vivaldi.net>
2021-08-08 00:13:42 +0200

    Denoise input for px grid && Refactoring

--- encoder.py
+++ encoder.py
@@ -13,6 +13,7 @@ from PIL import Image
 import ar_utils
 import importlib
 
+
 class Encoder(object):
     def __init__(self, output_path="intermediate_representation", method=ar_utils.methods[0], size=256, p=0, grid_size=10, plot=False) -> None:
         self.methods = ar_utils.methods
@@ -36,28 +37,28 @@ class Encoder(object):
 
     def main(self):
         parser = argparse.ArgumentParser(prog='Recolor Encoder',
-                                            description='Encodes images, to be decoded by Recolor')
+                                         description='Encodes images, to be decoded by Decoder')
 
         parser.add_argument('-o', '--output_path', action='store', dest='output_path', type=str,
-                               default='intermediate_representation',
-                               help='The path to the folder or file, where the grayscale version and color information will be written to')
+                            default='intermediate_representation',
+                            help='The path to the folder or file, where the grayscale version and color information will be written to')
         parser.add_argument('-i', '--input_path', action='store', dest='input_path', type=str, default='input_images',
-                               help='Path to individual image, or folder with images')
+                            help='Path to individual image, or folder with images')
         parser.add_argument('-m', '--method', action='store', dest='method', type=str, default=self.methods[0],
                             help='The colorization method to use. Possible values: \"' + ', '.join(self.methods) + '\"')
-        parser.add_argument('-w','--watch', dest='watch', help='watch input folder for new images', action='store_true')
+        parser.add_argument('-w', '--watch', dest='watch', help='watch input folder for new images', action='store_true')
 
         # for ideepcolor-px
         parser.add_argument('-s', '--size', action='store', dest='size', type=int, default=256,
-                               help='Size of the indermediate mask to store the color pixels. Power of 2. \
-                               The bigger, the more accurate the result, but requires more storage, and RAM capacity (decoder) \
-                               (For 2048 up to 21GB RAM)')
+                            help='Size of the indermediate mask to store the color pixels. Power of 2. \
+                            The bigger, the more accurate the result, but requires more storage, and RAM capacity (decoder) \
+                            (For 2048 up to 21GB RAM)')
         parser.add_argument('-g', '--grid_size', action='store', dest='grid_size', type=int, default=10,
-                               help='Spacing between color pixels in intermediate mask (--size)  1: fill every spot in mask.  0: dont use any color pixel ')
+                            help='Spacing between color pixels in intermediate mask (--size)  1: fill every spot in mask.  0: dont use any color pixel ')
         parser.add_argument('-p', '--p', action='store', dest='p', type=int, default=0,
-                               help='The "radius" the color values will have. \
-                               A higher value means one color pixel will later cover multiple gray pixels. Default: 0')
-        parser.add_argument('-plt','--plot', dest='plot', help='Generate Plots for visualization', action='store_true')
+                            help='The "radius" the color values will have. \
+                            A higher value means one color pixel will later cover multiple gray pixels. Default: 0')
+        parser.add_argument('-plt', '--plot', dest='plot', help='Generate Plots for visualization', action='store_true')
 
         args = parser.parse_args()
         self.watch = args.watch
@@ -93,20 +94,19 @@ class Encoder(object):
                     print("Warning: Found non image file: " + fil.path)
                     pass
 
-
-
-
     def load_image(self, path, colorspace="lab"):
         if colorspace == "lab":
             img_rgb = cv2.cvtColor(cv2.imread(path, 1), cv2.COLOR_BGR2RGB)
-            img_lab_fullres = color.rgb2lab(img_rgb).transpose((2, 0, 1))
-            return img_lab_fullres
+            img_lab = self.rgb_to_lab(img_rgb)
+            return img_lab
         elif colorspace == "rgb":
             img = cv2.cvtColor(cv2.imread(path, 1), cv2.COLOR_BGR2RGB)
             return img
+        elif "gray" in colorspace or "grey" in colorspace:
+            return cv2.cvtColor(cv2.imread(path, 1), cv2.COLOR_BGR2GRAY)
 
-    def load_image_to_gray(self, path):
-        return cv2.cvtColor(cv2.imread(path, 1), cv2.COLOR_BGR2GRAY)
+    def rgb_to_lab(self, rgb):
+        return color.rgb2lab(rgb).transpose((2, 0, 1))
 
     def encode(self, img_path):
         """
@@ -116,20 +116,17 @@ class Encoder(object):
         """
         self.image_path = img_path
         img_lab_fullres = self.load_image(img_path)
-        img_gray = self.load_image_to_gray(img_path)
+        img_gray = self.load_image(img_path, colorspace="gray")
         ar_utils.save_img(self.output_path, ar_utils.gen_new_gray_filename(img_path), img_gray)
-
-
-        # mask = ar_utils.Mask()
 
         if "ideepcolor-px" in self.method:
             filename_mask = ar_utils.gen_new_mask_filename(img_path)
             mask = None
             if self.method == "ideepcolor-px-grid":
-                mask = self.get_color_mask_grid(img_lab_fullres, self.grid_size, self.size, self.p)
+                mask = self.get_color_mask_grid(img_path, self.grid_size, self.size, self.p)
                 mask.save(self.output_path, os.path.basename(filename_mask), grid_size=self.grid_size)
             elif self.method == "ideepcolor-px-selective":
-                mask = self.get_color_mask_selective(img_lab_fullres, img_path)
+                mask = self.get_color_mask_selective(img_path)
                 mask.save(self.output_path, os.path.basename(filename_mask))
 
         elif self.method == "ideepcolor-global":
@@ -144,8 +141,6 @@ class Encoder(object):
             pass
         else:
             print("Error: method not valid:", self.method)
-
-
 
     def encode_ideepcolor_global(self, img_path, size) -> np.ndarray:
         os.environ['GLOG_minloglevel'] = '2' # suprress Caffe verbose prints
@@ -176,8 +171,14 @@ class Encoder(object):
         ar_utils.save_glob_dist(self.output_path, img_path, glob_dist_in)
         return glob_dist_in
 
-    
-    def get_color_mask_grid(self, img, grid_size=100, size=256, p=0):
+    def denoise_image_for_px_selection(self, rgb, k=5):
+        """
+        :param rgb: input image in rgb format
+        :return: median denoised image in rgb format
+        """
+        return cv2.medianBlur(rgb, k)
+
+    def get_color_mask_grid(self, img_path, grid_size=100, size=256, p=0):
         """
         :param img: original color image as lab (lab, y, x)
         :param grid_size: distance between pixels of grid in pixels 0 - mask size (-1: every space filled, 0: None filled (stock coloring))
@@ -185,12 +186,15 @@ class Encoder(object):
         """
         # TODO: save plot of grid
         mask = ar_utils.Mask(size=size, p=p)
+
+        rgb = self.load_image(img_path, colorspace="rgb")
+        rgb = self.denoise_image_for_px_selection(rgb, k=5)
+        img = self.rgb_to_lab(rgb)
         if grid_size == 0:
             return mask
 
         h = len(img[0])
         w = len(img[0][0])
-
 
         for y in range(size):
             if y % grid_size != 0:
@@ -199,46 +203,46 @@ class Encoder(object):
                 if x % grid_size != 0:
                     continue
                 y_img, x_img = ar_utils._coord_mask_to_img(h, w, y, x, size)
-                mask.put_point((y, x), [ img[1][y_img][x_img], img[2][y_img][x_img] ])
-
-        # mask.put_point([135,160], 3, [100,-69])
-        # print(mask.input_ab)
+                mask.put_point((y, x), [ img[1][y_img][x_img],
+                                         img[2][y_img][x_img] ])
         return mask
 
-
     # Everything for selective color mask
-
-    def get_color_mask_selective(self, img, img_path, round_to=10, scaling_factor=None):
-        from skimage import filters, color, restoration, util, transform
+    def get_color_mask_selective(self, img_path, round_to=10, scaling_factor=None):
+        """
+        :param:
+        :return Mask: Mask of pixels
+        """
+        from skimage import filters, restoration, util, transform
         # PARAM: hardcoded, round_to (for cityscapes rather smaller (8). Default: 10)
         # PARAM: hardcoded, scaling_factor: 8 for highres, or higher. 4, 2 for cityscapes and low res
+
+        # load as rgb 0-255
+        rgb = self.load_image(img_path, colorspace="rgb")
+
+        img_dims = rgb.shape[:-1]
+
         if not scaling_factor:
-            scaling_factor = int( min(img.shape[-2:])/250 ) # cityscapes(vga; w:480) -> 2, dragon_pool(w:2370) -> 9
+            scaling_factor = int(round( min(img_dims)/250 )) # cityscapes(vga; w:480) -> 2, dragon_pool(w:2370) -> 9
         print("Scaling factor: ", scaling_factor)
 
         mask = ar_utils.Mask(size=self.size, p=self.p)
-        # reload as rgb 0-255
-        rgb = self.load_image(img_path, colorspace="rgb")
-
+        
         # Median Filter; remove extreme individual noise pixels
-        # PARAM: k for median blur
-        k = 5
-        rgb = cv2.medianBlur(rgb, k)
+        # will be used for selection of pixels for mask later and also as first preprocessing step
+        rgb = self.denoise_image_for_px_selection(rgb, k=5)
 
-        # for picking out color values without noise later
         lab_median = self.rgb_to_lab(rgb)
         a_median = lab_median[1].astype(int)
         b_median = lab_median[2].astype(int)
         
         # scale down image
         img_resized = transform.resize(rgb,
-                                       (img.shape[1] // scaling_factor, img.shape[2] // scaling_factor),
+                                       (img_dims[0] // scaling_factor, img_dims[1] // scaling_factor),
                                        anti_aliasing=True)
 
         img_resized = self.rgb_to_lab(img_resized)
 
-        a_orig = img[1].astype(int)
-        b_orig = img[2].astype(int)
         L = img_resized[0].astype(int)
         a = img_resized[1].astype(int)
         b = img_resized[2].astype(int)
@@ -249,28 +253,28 @@ class Encoder(object):
 
         # Bilateral Filter; Edge preserving blur
         # PARAM: sigma_spatial, (/250)
-        sigma_spatial = min(img.shape[-1:]) / 500
+        sigma_spatial = min(img_dims) / 500
         print("Sigma Spatial (Bilateral)", sigma_spatial)
         # PARAM: sigma_color: sig-default*100
-        sigma_color = None#restoration.estimate_sigma(img_resized)*100
+        sigma_color = None  # restoration.estimate_sigma(img_resized)*100
         print("Sigma Color (Bilateral)", sigma_color)
         # img_resized = restoration.denoise_bilateral(img_resized, multichannel=True,
         #                                             sigma_spatial=sigma_spatial,
         #                                             sigma_color=sigma_color)
 
         a = restoration.denoise_bilateral(a.astype(float), multichannel=False,
-                                                    sigma_spatial=sigma_spatial,
-                                                    sigma_color=sigma_color)
+                                          sigma_spatial=sigma_spatial,
+                                          sigma_color=sigma_color)
         b = restoration.denoise_bilateral(b.astype(float), multichannel=False,
-                                                    sigma_spatial=sigma_spatial,
-                                                    sigma_color=sigma_color)
+                                          sigma_spatial=sigma_spatial,
+                                          sigma_color=sigma_color)
 
         # Gaussian blur; smooth out colors a bit more, reduces points overall
         # PARAM: calculated sigma
-        sigma = min(a.shape)/250 # Gaussian (/250)
+        sigma = min(a.shape)/250  # Gaussian (/250)
         print("Sigma Gaussian:", sigma)
         a = filters.gaussian(a, sigma, preserve_range=True)
-        b = filters.gaussian(b, sigma, preserve_range=True)        
+        b = filters.gaussian(b, sigma, preserve_range=True)
 
         # shift back to ab space -100-100
         a = util.img_as_ubyte(a.astype(int)).astype(int)-100
@@ -282,17 +286,16 @@ class Encoder(object):
 
         # delete points near edges, since sometimes they tend to bunch on the edges, and those are not super important for colorization anyway
         h, w = a.shape
-        dist = 2 # distance from edges
+        dist = 2  # distance from edges
         keep = np.ones(len(centres), dtype=bool)
         for idx, c in enumerate(centres):
             if c[0] < dist or c[0] > h-dist or c[1] < dist or c[1] > w-dist:
                 keep[idx] = False
         centres = centres[keep]
-        
+
         # Save image with red dots for selected pixels
         if self.plot:
             import matplotlib.pyplot as plt
-            rgb = self.lab_to_rgb(img)
             plt.imshow(rgb)
             y = np.array( [row[0] for row in centres] )*scaling_factor
             x = np.array( [row[1] for row in centres] )*scaling_factor
@@ -305,7 +308,7 @@ class Encoder(object):
             plt.close()
 
         # Use found interesting pixels as coordinates to fill mask
-        h, w = a_orig.shape
+        h, w = img_dims
         for px in centres:
             # scale up to resolution of input image
             loc = (px[0]*scaling_factor, px[1]*scaling_factor)
@@ -316,7 +319,7 @@ class Encoder(object):
 
         return mask
 
-    def round_arr_to(self, arr, r_to=20):
+    def round_arr_to(self, arr, r_to=10):
         """
         Rounds numpy array to nearest r_to
         """
@@ -351,8 +354,7 @@ class Encoder(object):
         """
         # replace pixel values via ff with unique id
         id_ = 0
-        # to make cv2.floodFill work
-        # ab = np.ascontiguousarray(ab, dtype=np.uint8)
+        # np.unique sorted ascendingly
         unique_colors = np.unique(ab)
         for col in unique_colors:
             # while as long as there is this color in the array
@@ -374,7 +376,7 @@ class Encoder(object):
         """
         import scipy.spatial.distance
         import random
-        ids =  np.unique(ab_ids)
+        ids = np.unique(ab_ids)
         centres = []
 
         for id in ids:
@@ -385,8 +387,8 @@ class Encoder(object):
             if len(area_coords_y) <= 4:
                 continue
             # get centre
-            centre = ( int((sum(area_coords_y)/len(area_coords_y))),
-                       int((sum(area_coords_x)/len(area_coords_x))) )
+            centre = ( int(round((sum(area_coords_y)/len(area_coords_y)))),
+                       int(round((sum(area_coords_x)/len(area_coords_x)))) )
 
             # since centre could be outside shape, search nearest point to centre
             closest = None
@@ -407,7 +409,7 @@ class Encoder(object):
 
             # if current blob is particularly large, use additional randomly selected pixels (+1 px per 1000 in blob)
             if len(area_coords_y) >= random_px_threshold:
-                add_px = int( len(area_coords_y) / random_px_threshold )
+                add_px = int(round( len(area_coords_y) / random_px_threshold ))
                 for i in range(add_px):
                     random.seed(0)
                     new_coord = random.randint(0, len(area_coords_y)-1)
@@ -415,9 +417,6 @@ class Encoder(object):
                     centres.append(new_px)
                     
         return np.array(centres)
-
-    def rgb_to_lab(self, rgb):
-        return color.rgb2lab(rgb).transpose((2, 0, 1))
 
     def lab_to_rgb(self, *args):
         """
